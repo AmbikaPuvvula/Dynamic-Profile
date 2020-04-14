c11d99c1c4b931d08947ef13ff55effbeb4c0c34
Ambika <ambikapuvvula@gmail.com>
2020-04-14 11:59:14 +0530

    Todays Class

---
+++ js/main.js
@@ -0,0 +1,50 @@
+fetch("data/data.json").then(response=>{
+	return response.json()
+}).then(data=>{
+	console.log(data);
+	profileInfo(data.profiles);
+}).catch(e=>{
+	console.error(Error);
+})
+
+var parent=document.getElementById("root");
+
+var profileInfo=info=>{
+	// for (i of info){
+	// 	console.log(info[i].profileInfo.name);
+	// 	console.log(i.profileInfo.email);// if we use "of" in place of "in".
+	// }
+	info.map((i,index)=>{
+		var child= document.createElement("article");
+		child.classList.add("child");
+
+		// Name
+		var name = document.createElement("h2");
+		name.textContent = i.profileInfo.name;
+		name.style.color="#588ae1";
+		name.style.textShadow="3px 3px 3px #ddd";
+		child.append(name);
+
+		// Email
+		var email = document.createElement("a");
+		email.textContent=i.profileInfo.email;
+		email.href="mailto:"+i.profileInfo.email;
+		child.append(email);
+		email.classList.add("email");
+		// Mobile
+		var mobile = document.createElement("a");
+		mobile.textContent=i.profileInfo.mobile;
+		mobile.href="tel:"+i.profileInfo.mobile;
+		mobile.classList.add("mobile")
+		child.append(mobile);
+
+		var button = document.createElement("a");
+		button.classList.add("btn");
+		button.href="resume.html?id="+index;
+		button.textContent="view Profile";
+		child.append(button);
+
+		parent.append(child);
+		// console.log(i.profileInfo.mobile);
+	})
+}
